830e02228ae519bf525e97fb315b3c2c00b70cd6
HP <HP@DESKTOP-G4J7E8F>
2022-07-29 18:46:04 +0530

    Add project files.

---
+++ StronglyTypedModelView/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedModelView.Models;
+
+namespace StronglyTypedModelView.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductData db = new ProductData();
+        // GET: Products
+        public ViewResult Index(int Pid)
+        {
+            //int prodid = 102;
+            //will search the product that are matching with your id
+            Product prod = db.ProductsList.Single(x => x.Pid == Pid);
+            return View(prod);
+        }
+        public ViewResult Index1()
+        {
+            List<Product> list = db.ProductsList.ToList();
+            return View(list);
+        }
+    }
+}
---
+++ StronglyTypedModelView/Models/ProductData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedModelView.Models
+{
+    public class ProductData
+    {
+        public IEnumerable<Product> ProductsList 
+        {
+            get
+            {
+                List<Product> products = new List<Product>()
+                {
+                    new Product() { Pid=101,Name="img 1",Price = 10.0 },
+                    new Product() { Pid=102,Name="img 2",Price = 12.0 },
+                    new Product() { Pid=103,Name="img 3",Price = 11.0 },
+                    new Product() { Pid=104,Name="img 4",Price = 13.0 }
+                };
+                return products;
+            }
+            
+        }  
+    }
+}
